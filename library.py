0d4aff81e08ada49b69b98a250bbb1eb3d291b0c
vekasni3172005-lgtm <vekasni3172005@gmail.com>
2026-01-08 18:00:17 +0530

    ver1

--- library.py
+++ library.py
@@ -1,1 +1,19 @@
-print("Welcome to Library Management System")
+import re
+
+# Read comma separated passwords
+passwords = input().split(",")
+
+valid_passwords = []
+
+for pwd in passwords:
+    if (6 <= len(pwd) <= 12 and
+        re.search("[a-z]", pwd) and
+        re.search("[A-Z]", pwd) and
+        re.search("[0-9]", pwd) and
+        re.search("[$#@]", pwd)):
+        valid_passwords.append(pwd)
+
+# Print valid passwords separated by comma
+print(",".join(valid_passwords))
+
+
